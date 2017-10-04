181349f877587e22292248f1f11cbc5ab072b815
Yoni Slotwiner <yonislots@gmail.com>
2017-10-04 18:48:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine){
     return "The line is currently empty.";
   } else {
     var array = [];
-    if(var i = 0; i < katzDeliLine.length; i++){
+    for(var i = 0; i < katzDeliLine.length; i++){
       array.push(i+1 + ". " + katzDeliLine[i]);
     }
     return ("The line is currently: " + array.join(", "));
